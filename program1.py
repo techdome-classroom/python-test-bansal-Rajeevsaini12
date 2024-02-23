0387c4c08c9d9c996ca3a46b8bea5b1bc09d3053
Rajeev Saini <88427910+Rajeevsaini12@users.noreply.github.com>
2024-02-23 13:32:59 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -6,13 +6,14 @@ class Solution(object):
         for char in s:
             if char in map:
                 top_ele=stack.pop() if stack else "#"
-                if map[char] !=top_ele:
+                if map[char] != top_ele:
                     return False
-
+            else:
+                stack.append(char)
         return True              
 
-
-print(isValid("{}"))
-print(isValid("()"))
-print(isValid("{)"))
+s=Solution()
+print(s.isValid("{}"))
+print(s.isValid("()"))
+print(s.isValid("{)"))
 
