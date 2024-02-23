79c81f79013cb902e6a25b44974d076d614afe9f
Rajeev Saini <88427910+Rajeevsaini12@users.noreply.github.com>
2024-02-23 13:27:47 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,7 +1,22 @@
 class Solution(object):
     def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        pass
+        rom_dict={ 'I':1, 'V':5,'X':10,'L':50, 'C':100, 'D':500 , 'M':1000}
+        total=0
+        pre_value=0
+
+        for char in s:
+            value=rom_dict[s]
+            if value>pre_value:
+                total+=value-2* pre_value
+
+            else:
+                total+=value
+            pre_value=value
+
+        return total   
+
+
+Sol=Solution()
+print(s.romanToInt('III'))
+print(s.romanToInt('LVIII'))
+print(s.romanToInt('MCMXCIV'))
