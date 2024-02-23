658ce0be7f4940b70d055b72f664dc46f418c0aa
Rajeev Saini <88427910+Rajeevsaini12@users.noreply.github.com>
2024-02-23 13:14:55 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,13 +1,18 @@
 class Solution(object):
     def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
-    
+        stack=[]
+        map={ ')':'(' , '}':'{', ']':'['}
+
+        for char in s:
+            if char in map:
+                top_ele=stack.pop() if stack else "#"
+                if map[char] !=top_ele:
+                    return False
+
+        return True              
 
 
-
-  
+print(isValid("{}"))
+print(isValid("()"))
+print(isValid("{)"))
 
